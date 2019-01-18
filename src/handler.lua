d456365acf317c6cb40bf83edfa5b4229d926a28
xujy <657154707@qq.com>
2019-01-18 11:34:05 +0800

    no message

--- src/handler.lua
+++ src/handler.lua
@@ -14,7 +14,7 @@ function ExternalAuthHandler:access(conf)
   client:set_timeouts(conf.connect_timeout, send_timeout, read_timeout)
 
   local res, err = client:request_uri(conf.url, {
-    method = kong.request.get_method(),
+    method = "post",
     path = kong.request.get_path(),
     query = kong.request.get_raw_query(),
     headers = kong.request.get_headers(),
@@ -22,11 +22,11 @@ function ExternalAuthHandler:access(conf)
   })
 
   if not res then
-    return kong.response.get_source()
+    return " http auth fail error "
   end
 
-  if res.status ~= 200 then
-    return kong.response.get_source()
+  if res.status = 200 then
+    return res
   end
 end
 
