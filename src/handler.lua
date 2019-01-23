67854cb2312dc915450d8afe3b55e153876a443b
xujy <657154707@qq.com>
2019-01-23 15:26:34 +0800

    no message

--- src/handler.lua
+++ src/handler.lua
@@ -27,9 +27,9 @@ function ExternalAuthHandler:access(conf)
       kong.log.err("response body1111: ", resp)
 
     if response_body["code"] then
-            kong.log.err("response codecode: ", response_body["code"])
+            kong.log.err("response codecode: ", table.concat(response_body["code"]))
             if response_body["error_code"] then
-                kong.log.err("response error_codeerror_code: ", response_body["error_code"])
+                kong.log.err("response error_codeerror_code: ", table.concat(response_body["error_code"]))
             end
     end
 
@@ -39,9 +39,9 @@ function ExternalAuthHandler:access(conf)
     end
 
     if response_body["code"] ~= 20101 then
-        kong.log.err("response code: ", response_body)
-        kong.log.err("response error_code1111111111: ", response_body["error_code"])
-        kong.log.err("response codecode2222222222222: ", response_body["code"])
+        kong.log.err("response code: ", table.concat(response_body))
+        kong.log.err("response error_code1111111111: ", table.concat(response_body["error_code"]))
+        kong.log.err("response codecode2222222222222: ", table.concat(response_body["code"]))
         return nil, resp
     end
 
